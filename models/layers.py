0dfe068e14a8a31f17d7a582b247a9439b71eff4
Aniruddh Krishna <166048909+AniruddhKrishna329@users.noreply.github.com>
2026-04-12 20:12:58 +0530

    Update layers.py

--- models/layers.py
+++ models/layers.py
@@ -3,17 +3,11 @@
 
 import torch
 import torch.nn as nn
-
-
 class CustomDropout(nn.Module):
-    """Custom Dropout layer.
-    """
-
     def __init__(self, p: float = 0.5):
         super().__init__()
         assert 0<=p<=1
-        self.p = p
-
+        self.p=p
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         if not self.training or self.p==0:
             return x
