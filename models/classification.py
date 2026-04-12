f7f0f3a719eeab17bf50fc1d498994304be952f3
Aniruddh Krishna <166048909+AniruddhKrishna329@users.noreply.github.com>
2026-04-12 20:12:28 +0530

    Update classification.py

--- models/classification.py
+++ models/classification.py
@@ -6,8 +6,6 @@ import torch.nn as nn
 from models.vgg11 import VGG11
 from models.layers import CustomDropout
 class VGG11Classifier(nn.Module):
-    """Full classifier = VGG11Encoder + ClassificationHead."""
-
     def __init__(self, num_classes: int = 37, in_channels: int = 3, dropout_p: float = 0.5):
         super().__init__()
         self.encoder=VGG11(in_channels)
@@ -21,7 +19,6 @@ class VGG11Classifier(nn.Module):
             nn.ReLU(inplace=True),
             CustomDropout(dropout_p),
             nn.Linear(4096,num_classes))
-
     def forward(self, x: torch.Tensor) -> torch.Tensor:
        x=self.encoder(x)
        return self.head(x)
