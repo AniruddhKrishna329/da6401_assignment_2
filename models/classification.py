e960b1937e5912527f7e20d60a899b8848e7ab2d
Aniruddh Krishna <166048909+AniruddhKrishna329@users.noreply.github.com>
2026-04-12 20:27:20 +0530

    Update classification.py

--- models/classification.py
+++ models/classification.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 from models.vgg11 import VGG11
 from models.layers import CustomDropout
-class VGG11Classifier(nn.Module):
+class VGG11Classifier(nn.Module): #classifies an image into one of the 37 breeds
     def __init__(self, num_classes: int = 37, in_channels: int = 3, dropout_p: float = 0.5):
         super().__init__()
         self.encoder=VGG11(in_channels)
@@ -19,6 +19,6 @@ class VGG11Classifier(nn.Module):
             nn.ReLU(inplace=True),
             CustomDropout(dropout_p),
             nn.Linear(4096,num_classes))
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor: #forward pass
        x=self.encoder(x)
        return self.head(x)
